f5be9c2e40ed6de69197702f4467945d8e1ce492
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2022-05-01 14:54:40 +0300

    Test
    testing the map

--- src/bg/tu_varna/sit/Main.java
+++ src/bg/tu_varna/sit/Main.java
@@ -1,10 +1,16 @@
 package bg.tu_varna.sit;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Jedi jedi  = new Jedi("Vader", Rank.BATTLE_MASTER, 12,"light",1999.999);
-        System.out.println(jedi);
+        Jedi vader = new Jedi("Vader", Rank.BATTLE_MASTER, 12, "light", 1999.999);
+        Jedi luke = new Jedi("luke", Rank.PADAWAN, 2, "light", 1653.5124);
+        Jedi obi = new Jedi("obi", Rank.PADAWAN, 2, "light", 1653.5124);
+        Planet embo = new Planet("embo");
+        Planet planet = new Planet("Corusant");
+        planet.addJediToPlanet(planet, vader);
+        planet.addJediToPlanet(planet, luke);
+        planet.getMap();
     }
 
 }
