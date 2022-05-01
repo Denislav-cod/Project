4b0c4b4ed30f2d0775a05cf701ef6875bc93296a
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2022-05-01 14:54:29 +0300

    Map and getMap
    Added method for map

--- src/bg/tu_varna/sit/Planet.java
+++ src/bg/tu_varna/sit/Planet.java
@@ -1,10 +1,12 @@
 package bg.tu_varna.sit;
 
+import java.util.*;
 public class Planet {
     private String name;
-
+    private HashMap<Planet, List<Jedi>> map;
     public Planet(String name) {
         this.name = name;
+        this.map = new HashMap<Planet, List<Jedi>>();
     }
 
     public String getName() {
@@ -14,4 +16,33 @@ public class Planet {
     public void setName(String name) {
         this.name = name;
     }
+
+    public HashMap<Planet, List<Jedi>> getMap() {
+        map.forEach((k,v) -> {
+            System.out.println( k + "Jedis");
+            System.out.println("\n"+ v);
+        });
+        return map;
+    }
+
+    public void addJediToPlanet(Planet planet, Jedi jedi) {
+        List<Jedi> jedis = map.get(planet);
+
+        // if list does not exist create it
+        if (jedis == null) {
+            jedis = new ArrayList<Jedi>();
+            jedis.add(jedi);
+            map.put(planet, jedis);
+        } else {
+            // add if item is not already in list
+            if (!jedis.contains(jedi)) jedis.add(jedi);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Planet" +
+                " with name " + name + " and population of ";
+    }
+
 }
